115b06308fe2ef8ea10e4e2e48384ed8820ab1d0
ThebeRising <tl.kgabo@gmail.com>
2017-06-19 15:20:24 +0200

    publish code

---
+++ OceaniaIIS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OceaniaIIS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OceaniaIIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OceaniaIIS.MvcApplication" Language="C#" %>
